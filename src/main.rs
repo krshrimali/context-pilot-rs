455220c3f92c53c6d3a46ed97de5e02996738446
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-06-17 17:16:16 +0530

    tests for get_all_files (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::ReadDir;
+use std::iter::Flatten;
 use std::path::Path;
 use std::path::PathBuf;
 
@@ -42,7 +44,26 @@ fn call_command_unique_files(file_path: &PathBuf) {
     );
 }
 
+fn get_all_files(folder_path: &Path) -> Vec<PathBuf> {
+    let mut output_files: Vec<PathBuf> = [].to_vec();
+    let flattened_read_dir = folder_path
+        .read_dir()
+        .expect("Unable to read from given path")
+        .flatten();
+    for each_dir in flattened_read_dir {
+        let possible_dir_path = each_dir.path();
+        if possible_dir_path.is_dir() {
+            // iterate through it again
+            output_files.append(&mut get_all_files(&possible_dir_path));
+        } else {
+            output_files.push(possible_dir_path);
+        }
+    }
+    output_files
+}
+
 fn walk(workspace_path_buf: &Path) {
+    let all_files = get_all_files(workspace_path_buf);
     for each_file in workspace_path_buf
         .read_dir()
         .expect("Unable to read directory from path each_file (TODO)")
@@ -90,6 +111,7 @@ fn main() -> CliResult {
     //     .read_dir()
     //     .expect("Couldn't open the directory, something went wrong...")
     // {
+    // TODO: Ideally, pass a function (something like func pointer) to walk along
     walk(&current_folder_path);
 
     Ok(())
@@ -129,4 +151,45 @@ fn main_sync() -> CliResult {
         println!("{:?}", output);
     }
     Ok(())
+}
+
+mod test {
+    use std::path::{Path, PathBuf};
+
+    use crate::get_all_files;
+    use std::fs::{File, ReadDir};
+    use std::io::{Error, Write};
+
+    fn create_sample_testing_dir() {
+        let output_folder_path = Path::new(".test");
+        if !output_folder_path.is_dir() {
+            std::fs::create_dir(output_folder_path).expect("Unable to create a directory");
+        }
+        let test_content = "test_content".to_string();
+        let total_files: usize = 6;
+
+        for idx in 1..total_files {
+            let file_path = output_folder_path.to_str().unwrap().to_owned() + "/test_path" + &idx.to_string() + ".txt";
+            let mut output_file =
+                File::create(file_path).expect("File wasn't successfully created");
+            write!(output_file, "{}", test_content).expect("Unable to write content to the file");
+        }
+    }
+
+    #[test]
+    fn test_get_all_files() {
+        create_sample_testing_dir();
+        let mut all_files = get_all_files(Path::new(".test/"));
+        all_files.sort();
+        assert_eq!(
+            all_files,
+            vec![
+                Path::new(".test/test_path1.txt").to_path_buf(),
+                Path::new(".test/test_path2.txt").to_path_buf(),
+                Path::new(".test/test_path3.txt").to_path_buf(),
+                Path::new(".test/test_path4.txt").to_path_buf(),
+                Path::new(".test/test_path5.txt").to_path_buf(),
+            ]
+        );
+    }
 }
