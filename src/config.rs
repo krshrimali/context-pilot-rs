2b82b5b6ade2721bfbb3d38b28dc3b50351ac0a2
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-07-05 19:24:50 +0530

    Init commit to create a PR and add design stuff

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 pub const LAST_MANY_COMMIT_HASHES: i32 = 5;
-pub const AUTHOR_DB_PATH: &str = "db_common.json";
-pub const FILE_DB_PATH: &str = "db_common.json";
+// pub const AUTHOR_DB_PATH: &str = "db_common.json";
+// pub const FILE_DB_PATH: &str = "db_common.json";
 pub const DB_FOLDER: &str = ".context_pilot_db";
+pub const DB_MAPPING_FILE: &str = "db_mapping.json";
