aae76bb399174b155de7d2c6992c39aa7e21a73e
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-10-20 21:14:01 +0530

    Slowly reaching to a point where this will start looking more beatufiul, oh and also usable xD

--- src/db_new.rs
+++ src/db_new.rs
@@ -1,5 +1,6 @@
 // use crate::{config, contextgpt_structs::AuthorDetails};
 
+use std::io::Write;
 use std::{collections::HashMap, fs::File, path::Path};
 
 // use simple_home_dir::home_dir;
@@ -7,15 +8,16 @@ use std::{collections::HashMap, fs::File, path::Path};
 use crate::{config, contextgpt_structs::AuthorDetails};
 
 type DBType = HashMap<String, HashMap<usize, Vec<AuthorDetails>>>;
+type MappingDBType = HashMap<String, usize>;
 
 // index; folder_path; currLines;
 #[derive(Default)]
 pub struct DB {
-    pub index: u32,
-    pub folder_path: String,
-    pub curr_lines: u32,
-    pub mapping_file_name: String,
-    pub current_data: DBType,
+    pub index: u32,                // The line of code that you are at, right now? TODO:
+    pub folder_path: String, // Current folder path that this DB is processing, or the binary is running
+    pub curr_lines: u32,     // TODO:
+    pub mapping_file_name: String, // This is for storing which file is in which folder/file? <-- TODO:
+    pub current_data: DBType, // The data that we have from the loaded DB into our inhouse member
 }
 
 impl DB {
@@ -29,6 +31,7 @@ impl DB {
             return data;
         } else {
             // TODO: Add a log that the DB doesn't exist
+            // TODO: Enable logging into a logging file and add two modes: debug and info
             return HashMap::new();
         }
     }
@@ -37,13 +40,18 @@ impl DB {
     pub fn init_db(&mut self, curr_file_path: &str) {
         // let folder_path = Path::new(simple_home_dir::home_dir().unwrap().to_str().unwrap())
         //     .join(config::DB_FOLDER);
-        let db_folder = config::DB_FOLDER;
+        // let db_folder = config::DB_FOLDER.to_owned() + &self.folder_path;
+        let db_folder = format!("{}/{}", config::DB_FOLDER, self.folder_path);
+        // let mut main_folder_path = String::new();
 
         if let Some(home) = simple_home_dir::home_dir() {
             let folder_path = home.join(db_folder);
 
             if let Some(path_str) = folder_path.to_str() {
                 self.folder_path = path_str.to_string();
+            } else {
+                eprintln!("Something went wrong while trying to get the string for the path");
+                return;
             }
         } else {
             eprintln!("Failed to determine the home directory");
@@ -51,35 +59,53 @@ impl DB {
         }
         self.index = 0;
         self.curr_lines = 0;
-        self.mapping_file_name = String::from("mapping.json");
+        // self.mapping_file_name = String::from("mapping.json");
 
         // Now initialise all relevant folders/files
-        std::fs::create_dir_all(self.folder_path).expect(&format!(
+        // When the child folders are also not present - we just want to iteratively create all folders
+        std::fs::create_dir_all(&self.folder_path).expect(&format!(
             "Unable to create folder for: {}",
             self.folder_path
         ));
 
         // Search for the index
         let db_file_index = self.find_index(curr_file_path);
+        // Filename will be: <db_file_index>.json
         if let Some(valid_index) = db_file_index {
-            self.index = *valid_index;
-            let db_file_path = format!("{}/{}", self.folder_path, self.index);
+            self.index = valid_index;
+            let db_file_path = format!("{}/{}.json", self.folder_path, self.index);
             let db_obj = Path::new(&db_file_path);
             if !db_obj.exists() {
-                File::create(db_obj).expect("Couldn't create the file for some reason");
+                File::create(db_obj).expect("Couldn't find the DB file");
                 self.current_data = HashMap::new();
                 return;
             }
             self.current_data = self.read();
         } else {
             self.current_data = HashMap::new();
-            return;
+            // return;
         }
     }
 
-    fn find_index(&mut self, curr_file_path: &str) -> Option<&u32> {
+    fn find_index(&mut self, curr_file_path: &str) -> Option<u32> {
+        // In each folder -> we'll have a mapping file which contains which filename corresponds to which index (to be used in the DB file)
+        self.mapping_file_name = "mapping.json".to_string();
         let mapping_file_path = format!("{}/{}", self.folder_path, self.mapping_file_name);
-        let mapping_data = std::fs::read_to_string(mapping_file_path).expect(&format!(
+        let mapping_path_obj = Path::new(&mapping_file_path);
+        if !mapping_path_obj.exists() {
+            // mapping file doesn't exist yet... we'll create one with the index as 0 for the given curr_file_path
+            let mut data: MappingDBType = HashMap::new();
+            data.insert(curr_file_path.to_string(), 0);
+            // let mut data = format!("{{}: 0}", curr_file_path);
+            let init_mapping_string =
+                serde_json::to_string_pretty(&data).expect("Unable to create data");
+            let mut mapping_path_file =
+                File::create(mapping_file_path).expect("Couldn't create this new file...");
+            write!(mapping_path_file, "{}", init_mapping_string)
+                .expect("Couldn't write a very simple data object into a new file...wow!");
+            return None;
+        }
+        let mapping_data = std::fs::read_to_string(&mapping_file_path).expect(&format!(
             "Unable to read the mapping file into string, file path: {}",
             mapping_file_path
         ));
@@ -88,6 +114,6 @@ impl DB {
                 "Unable to deserialize the mapping file, path: {}",
                 mapping_file_path
             ));
-        return mapping_json.get(curr_file_path);
+        mapping_json.get(curr_file_path).copied()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod algo_loc;
+// mod algo_loc;
 mod authordetails_impl;
 mod config;
 mod contextgpt_structs;
-mod db;
+// mod db;
 mod db_new;
 mod git_command_algo;
 
@@ -10,10 +10,10 @@ use linecount::count_lines;
 use quicli::prelude::*;
 use structopt::StructOpt;
 
-use algo_loc::get_unique_files_changed;
+// use algo_loc::get_unique_files_changed;
 use contextgpt_structs::{Cli, RequestTypeOptions};
 
-use crate::algo_loc::get_contextual_authors;
+// use crate::algo_loc::get_contextual_authors;
 
 // #[warn(dead_code)]
 fn main() -> CliResult {
@@ -32,35 +32,42 @@ fn main() -> CliResult {
     } else {
         end_line_number
     };
-    let mut auth_db_obj = db::DB {
-        db_file_name: config::AUTHOR_DB_PATH.to_string(),
-        ..Default::default()
-    };
-    let mut file_db_obj = db::DB {
-        db_file_name: config::FILE_DB_PATH.to_string(),
+    // let mut db_obj = db_new::DB {
+    //     db_file_name: config::AUTHOR_DB_PATH.to_string(),
+    //     ..Default::default()
+    // };
+    // let mut db_obj = db::DB {
+    //     db_file_name: config::FILE_DB_PATH.to_string(),
+    //     ..Default::default()
+    // };
+    let mut db_obj = db_new::DB {
+        folder_path: args.folder_path,
         ..Default::default()
     };
     match args.request_type {
         RequestTypeOptions::File => {
-            file_db_obj.init_db();
-            let output = get_unique_files_changed(
-                args.file,
-                &args.start_number,
-                &valid_end_line_number,
-                &mut file_db_obj,
-            );
-            println!("{:?}", output);
+            db_obj.init_db(args.file.as_str());
+            println!("Initialised for File");
+            // let output = get_unique_files_changed(
+            //     args.file,
+            //     &args.start_number,
+            //     &valid_end_line_number,
+            //     &mut db_obj,
+            // );
+            // println!("{:?}", output);
         }
         RequestTypeOptions::Author => {
-            auth_db_obj.init_db();
-            let output = get_contextual_authors(
-                args.file,
-                &args.start_number,
-                &valid_end_line_number,
-                &mut auth_db_obj,
-            );
-            println!("{:?}", output);
+            db_obj.init_db(args.file.as_str());
+            println!("Initialised for author");
+            // let output = get_contextual_authors(
+            //     args.file,
+            //     &args.start_number,
+            //     &valid_end_line_number,
+            //     &mut db_obj,
+            // );
+            // println!("{:?}", output);
         }
     };
     Ok(())
 }
+
