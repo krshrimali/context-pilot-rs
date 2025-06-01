904d8725517f5ab055057e8e29fbc7fca5d7dbea
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-06-01 17:52:24 +0530

    Comment out accuracy part

--- src/git_command_algo.rs
+++ src/git_command_algo.rs
@@ -142,91 +142,92 @@ pub async fn extract_details_parallel(file_path: String) -> HashMap<u32, AuthorD
         };
         auth_details_map.insert(*line_number, author_details.clone());
     }
-    let mut total_count = 0;
-    let mut failed_count = 0;
-    // Find accuracy of the indexing:
-    // Accuracy is defined as, as the output for each line of code - the last commit should always
-    // be coming from git blame.
-    for (line_number, line_detail) in map.iter() {
-        if line_detail.get(0).unwrap().content.is_empty() {
-            continue;
-        }
-        // Find the git blame from the line_number:
-        let mut command = Command::new("git");
-        command.args([
-            "blame",
-            "-L",
-            &format!("{},{}", line_number, line_number),
-            "--abbrev=7",
-            "--",
-            file_path.as_str(),
-        ]);
-        let output = command
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .output()
-            .unwrap();
-        let stdout_buf = String::from_utf8(output.stdout).unwrap();
-        // Extract commit hash from: c5bca082 (Kushashwa Ravi Shrimali 2023-10-21 16:52:43 +0530 1) mod algo_loc;
-        let mut commit_hash = String::new();
-        if let Some(first_line) = stdout_buf.lines().next() {
-            // Split by space and take the first part as commit hash.
-            let parts: Vec<&str> = first_line.split_whitespace().collect();
-            if !parts.is_empty() {
-                commit_hash = parts[0].to_string();
-            }
-        }
-        // Check if commit hash == author_details_vec
-        let author_detail = auth_details_map.get(line_number);
-        if let Some(author_detail) = author_detail {
-            // If the commit hash is not already in the commit_hashes, add it.
-            if commit_hash.starts_with("^") {
-                // Make sure this is included as well...
-                let commit_hash = commit_hash.strip_prefix("^").unwrap();
-                if author_detail
-                    .commit_hashes
-                    .contains(&commit_hash.to_string())
-                {
-                    if author_detail
-                        .commit_hashes
-                        .contains(&commit_hash.to_string())
-                    {
-                        total_count += 1;
-                    } else {
-                        failed_count += 1;
-                    }
-                }
-            } else {
-                // Just take 7 first chars:
-                if commit_hash.len() > 7 {
-                    commit_hash = commit_hash[..7].to_string();
-                } else {
-                    continue;
-                }
-                // let commit_hash = &commit_hash[..7];
-                // println!("Searching for commit hash: {}", commit_hash);
-                if author_detail
-                    .commit_hashes
-                    .contains(&commit_hash.to_string())
-                {
-                    total_count += 1;
-                } else {
-                    failed_count += 1;
-                    println!(
-                        "Commit hash {} not found in author details for line {}",
-                        commit_hash, line_number
-                    );
-                    println!("Author details: {:?}", author_detail.commit_hashes);
-                }
-            }
-        }
-    }
-    println!(
-        "Accuracy for file {} : {}/{}",
-        file_path.clone(),
-        total_count,
-        total_count + failed_count
-    );
+    // FIXME: @krshrimali - Remove this once proper testing is done.
+    // let mut total_count = 0;
+    // let mut failed_count = 0;
+    // // Find accuracy of the indexing:
+    // // Accuracy is defined as, as the output for each line of code - the last commit should always
+    // // be coming from git blame.
+    // for (line_number, line_detail) in map.iter() {
+    //     if line_detail.get(0).unwrap().content.is_empty() {
+    //         continue;
+    //     }
+    //     // Find the git blame from the line_number:
+    //     let mut command = Command::new("git");
+    //     command.args([
+    //         "blame",
+    //         "-L",
+    //         &format!("{},{}", line_number, line_number),
+    //         "--abbrev=7",
+    //         "--",
+    //         file_path.as_str(),
+    //     ]);
+    //     let output = command
+    //         .stdout(Stdio::piped())
+    //         .stderr(Stdio::piped())
+    //         .output()
+    //         .unwrap();
+    //     let stdout_buf = String::from_utf8(output.stdout).unwrap();
+    //     // Extract commit hash from: c5bca082 (Kushashwa Ravi Shrimali 2023-10-21 16:52:43 +0530 1) mod algo_loc;
+    //     let mut commit_hash = String::new();
+    //     if let Some(first_line) = stdout_buf.lines().next() {
+    //         // Split by space and take the first part as commit hash.
+    //         let parts: Vec<&str> = first_line.split_whitespace().collect();
+    //         if !parts.is_empty() {
+    //             commit_hash = parts[0].to_string();
+    //         }
+    //     }
+    //     // Check if commit hash == author_details_vec
+    //     let author_detail = auth_details_map.get(line_number);
+    //     if let Some(author_detail) = author_detail {
+    //         // If the commit hash is not already in the commit_hashes, add it.
+    //         if commit_hash.starts_with("^") {
+    //             // Make sure this is included as well...
+    //             let commit_hash = commit_hash.strip_prefix("^").unwrap();
+    //             if author_detail
+    //                 .commit_hashes
+    //                 .contains(&commit_hash.to_string())
+    //             {
+    //                 if author_detail
+    //                     .commit_hashes
+    //                     .contains(&commit_hash.to_string())
+    //                 {
+    //                     total_count += 1;
+    //                 } else {
+    //                     failed_count += 1;
+    //                 }
+    //             }
+    //         } else {
+    //             // Just take 7 first chars:
+    //             if commit_hash.len() > 7 {
+    //                 commit_hash = commit_hash[..7].to_string();
+    //             } else {
+    //                 continue;
+    //             }
+    //             // let commit_hash = &commit_hash[..7];
+    //             // println!("Searching for commit hash: {}", commit_hash);
+    //             if author_detail
+    //                 .commit_hashes
+    //                 .contains(&commit_hash.to_string())
+    //             {
+    //                 total_count += 1;
+    //             } else {
+    //                 failed_count += 1;
+    //                 println!(
+    //                     "Commit hash {} not found in author details for line {}",
+    //                     commit_hash, line_number
+    //                 );
+    //                 println!("Author details: {:?}", author_detail.commit_hashes);
+    //             }
+    //         }
+    //     }
+    // }
+    // println!(
+    //     "Accuracy for file {} : {}/{}",
+    //     file_path.clone(),
+    //     total_count,
+    //     total_count + failed_count
+    // );
     auth_details_map
 }
 
